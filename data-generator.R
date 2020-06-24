7f59c6b8ad838f6a8c797b1bb42dd5cb27e80ac6
witsyke <witzke.simon@gmail.com>
2020-06-24 17:11:15 +0200

    Creates function to load recreatable samples

--- data-generator.R
+++ data-generator.R
@@ -6,31 +6,31 @@ source("./helper.R")
 load("./nets/mehra-complete.rda")
 
 # define sample size
-sample.size <- 10000000
+sample.size <- 51000000
 
 # gather observations from original net
 sim.data <- rbn(bn, sample.size)
 
 # convert timestamps to continuous
-clean.sim.data <- sim.data %>%
+data <- sim.data %>%
   mutate(DateTime = chron(dates = paste(Year, Month, Day, sep="-"), times = paste(Hour, "0", "0", sep=":"), format = c('y-m-d','h:m:s'))) %>%
   filter(!is.na(DateTime)) %>%
   select(-Year, -Month, -Day, -Hour)
 
 # save base data set for further use
-save(clean.sim.data,file="base-data.rds", compress = T)
+save(data,file="base-data.rds")
 
 # blacklist arcs to DateTime, Region, Zone, Long, Lat, Alt
 # and from CVD60
 source.nodes <- c("DateTime", "Region", "Zone", "Type", "Latitude", "Altitude", "Longitude")
 
-bl.source <- names(clean.sim.data) %>%
+bl.source <- names(data) %>%
   crossing(source.nodes) %>%
   rename("from" = ".", "to" = "source.nodes")
 
 bl.sink <- c("CVD60") %>%
-  crossing(names(clean.sim.data)) %>%
-  rename("from" = ".", "to" = "names(clean.sim.data)")
+  crossing(names(data)) %>%
+  rename("from" = ".", "to" = "names(data)")
 
 bl <- dplyr::union(bl.source, bl.sink)
 
--- helper.R
+++ helper.R
@@ -1,3 +1,15 @@
+library(tidyverse)
+
 timestamped.filename <- function(name) {
   return(paste(format(Sys.time(), "%Y%m%d-%H%M%S"), name, sep='-'))
 }
+
+generate.sample <- function(size, seed = 0, file.name = "./base-data.rds"){
+  set.seed(seed)
+  
+  load(file.name)
+  
+  sample_n(data, size = size, replace = F)
+  
+}
+
